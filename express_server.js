6b8a75df830dcac69088a4c14d095f3080abce87
Simon Huang <firebfm@gmail.com>
2020-12-10 00:29:36 +0000

    get /urls/new only logged in users can view

--- express_server.js
+++ express_server.js
@@ -74,8 +74,12 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = { user: req.cookies["user_id"] };
-  res.render("urls_new", templateVars);
+  if (req.cookies["user_id"]) {
+    const templateVars = { user: req.cookies["user_id"] };
+    res.render("urls_new", templateVars);
+  } else {
+    res.redirect('/login');
+  }
 });
 
 app.get("/urls/:shortURL", (req, res) => {
